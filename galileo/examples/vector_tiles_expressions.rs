7ded9a1337f3001cddb68f04dda5ade073c187a8
shri-acha <happy.irhs@gmail.com>
2026-01-02 10:24:04 +0545

    refactor: replaced use of deprecated method 'with_react_tile_size'

--- galileo/examples/vector_tiles_expressions.rs
+++ galileo/examples/vector_tiles_expressions.rs
@@ -206,7 +206,7 @@ fn exponential_interpolation_style() -> StyleRule {
 
 fn tile_schema() -> TileSchema {
     TileSchemaBuilder::web_mercator(2..16)
-        .with_rect_tile_size(1024)
+        .rect_tile_size(1024)
         .build()
         .expect("invalid tile schema")
 }
