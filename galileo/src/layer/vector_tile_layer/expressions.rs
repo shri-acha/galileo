32b10acab9e80602502fa44feee426cf53db7304
irhs0 <happy.irhs@gmail.com>
2026-01-22 16:44:52 +0545

    Added support for cubic interpolation (#301)
    * feat: Added cubic interpolation
    
    * feat: replaced nr method with bisection
    
    * add: tests for cubic_interpolation

--- galileo/src/layer/vector_tile_layer/expressions.rs
+++ galileo/src/layer/vector_tile_layer/expressions.rs
@@ -1,5 +1,5 @@
 //! Module that provides support for step and interpolate expressions for Color and Number
-//! typesexpresion
+//! type expression
 use std::cmp::Ordering;
 use std::collections::BTreeSet;
 use std::vec::IntoIter;
@@ -16,6 +16,9 @@ pub enum InterpolationArgs<T> {
     /// Exponential variant of interpolation arguments
     #[serde(rename = "exponential")]
     Exponential(ExponentialInterpolationArgs<T>),
+    /// Cubic variant of interpolation arguments
+    #[serde(rename = "cubic")]
+    Cubic(CubicInterpolationArgs<T>),
 }
 
 impl<T: Copy> InterpolationArgs<T> {
@@ -23,6 +26,7 @@ impl<T: Copy> InterpolationArgs<T> {
         match self {
             Self::Linear(args) => &args.step_values,
             Self::Exponential(args) => &args.step_values,
+            Self::Cubic(args) => &args.step_values,
         }
     }
 }
@@ -63,6 +67,29 @@ impl<T: Copy> ExponentialInterpolationArgs<T> {
         }
         Ok(Self {
             base,
+            step_values: step_values.into_iter().collect::<BTreeSet<_>>(),
+        })
+    }
+}
+
+/// Arguments for Cubic Interpolation Function
+#[derive(Clone, Debug, Serialize, PartialEq, Deserialize)]
+pub struct CubicInterpolationArgs<T> {
+    control_points: [f64; 4],
+    step_values: BTreeSet<StepValue<T>>,
+}
+
+impl<T: Copy> CubicInterpolationArgs<T> {
+    /// Returns a new instance of `CubicInterpolationArgs`
+    pub fn new(
+        control_points: [f64; 4],
+        step_values: IntoIter<StepValue<T>>,
+    ) -> Result<Self, String> {
+        if step_values.len() < 2 {
+            return Err("At least 2 step values required".to_string());
+        }
+        Ok(Self {
+            control_points,
             step_values: step_values.into_iter().collect::<BTreeSet<_>>(),
         })
     }
@@ -287,6 +314,14 @@ impl<T: InterpolatableValue> InterpolateExpression<T> {
                     current_resolution,
                     args.base,
                 ),
+                InterpolationArgs::Cubic(args) => cubic_interpolation(
+                    rv_range.min_resolution,
+                    rv_range.max_resolution,
+                    start,
+                    end,
+                    current_resolution,
+                    args.control_points,
+                ),
             };
 
             result.set_component(&component, component_value);
@@ -294,6 +329,60 @@ impl<T: InterpolatableValue> InterpolateExpression<T> {
 
         result
     }
+}
+
+fn bisection_solve(f: impl Fn(f64) -> f64, mut low: f64, mut high: f64, eps: f64) -> f64 {
+    let mut t = low;
+    const MAX_ITERS: u32 = 25;
+    for _ in 0..MAX_ITERS {
+        t = low + (high - low) / 2.0;
+        let val = f(t);
+        if val.abs() < eps {
+            return t;
+        }
+        if val > 0.0 {
+            high = t;
+        } else {
+            low = t;
+        }
+    }
+    t
+}
+
+fn inv_bezier(x0: f64, cpts: [f64; 4]) -> f64 {
+    let x1 = cpts[0];
+    let x2 = cpts[2];
+    // Bx(t)
+    let f = move |t: f64| {
+        3. * (1. - t).powi(2) * t * x1 + 3. * (1. - t) * t.powi(2) * x2 + t.powi(3) - x0
+    };
+    bisection_solve(f, 0., 1., 0.001)
+}
+
+///  Cubic bezier interpolation solver.
+///
+/// The math formula for algorithm is:
+///   - `Bx(t) = 3(1-t)²t·x1 + 3(1-t)t²·x2 + t³`
+///   - `By(t) = 3(1-t)²t·y1 + 3(1-t)t²·y2 + t³`
+///     Implementation is taken from: https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+///
+fn cubic_interpolation(
+    x_start: f64,
+    x_end: f64,
+    y_start: f64,
+    y_end: f64,
+    x0: f64,
+    control_points: [f64; 4],
+) -> f64 {
+    let x_normalized =
+        ((x0 - x_start) / (x_end - x_start).clamp(f64::EPSILON, f64::MAX)).clamp(0., 1.);
+    // inverse of Bx(t) for x_normalized
+    let t = inv_bezier(x_normalized, control_points);
+    let y1 = control_points[1];
+    let y2 = control_points[3];
+    // By(t)
+    let y_normalized = 3. * (1. - t).powi(2) * t * y1 + 3. * (1. - t) * t.powi(2) * y2 + t.powi(3);
+    y_start + y_normalized * (y_end - y_start)
 }
 
 fn exponential_interpolation(
@@ -315,7 +404,7 @@ fn exponential_interpolation(
 
     let offset = y_end - y_start;
 
-    (y_start + t * (offset)).clamp(0.0, 255.0)
+    y_start + t * (offset)
 }
 
 fn linear_interpolation(x_start: f64, x_end: f64, y_start: f64, y_end: f64, x0: f64) -> f64 {
@@ -565,6 +654,278 @@ mod number_tests {
 
         assert_eq!(expr.evaluate(25.0).round(), 41.0);
         assert_eq!(expr.evaluate(60.0).round(), 132.0);
+    }
+
+    #[test]
+    fn cubic_interpolation_bounds() {
+        let args: CubicInterpolationArgs<Color> = CubicInterpolationArgs::new(
+            [0.0, 1.0, 0.5, 0.75],
+            vec![
+                StepValue {
+                    resolution: 50.0,
+                    step_value: Color::rgba(128, 128, 128, 128),
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: Color::rgba(200, 200, 200, 200),
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(0.0), Color::rgba(128, 128, 128, 128));
+        assert_eq!(expr.evaluate(100.0), Color::rgba(200, 200, 200, 200));
+    }
+
+    #[test]
+    fn cubic_interpolation_bounds_f64() {
+        let args: CubicInterpolationArgs<Color> = CubicInterpolationArgs::new(
+            [0.0, 1.0, 0.5, 0.75],
+            vec![
+                StepValue {
+                    resolution: 50.0,
+                    step_value: Color::rgba(128, 128, 128, 128),
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: Color::rgba(200, 200, 200, 200),
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(0.0), Color::rgba(128, 128, 128, 128));
+        assert_eq!(expr.evaluate(100.0), Color::rgba(200, 200, 200, 200));
+    }
+
+    #[test]
+    fn cubic_interpolation() {
+        let args: CubicInterpolationArgs<Color> = CubicInterpolationArgs::new(
+            [0.0, 1.0, 0.5, 0.75],
+            vec![
+                StepValue {
+                    resolution: 0.0,
+                    step_value: Color::rgba(0, 0, 0, 0),
+                },
+                StepValue {
+                    resolution: 50.0,
+                    step_value: Color::rgba(128, 128, 128, 128),
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: Color::rgba(200, 200, 200, 200),
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0), Color::rgba(108, 108, 108, 108));
+        assert_eq!(expr.evaluate(60.0), Color::rgba(186, 186, 186, 186));
+    }
+
+    #[test]
+    fn cubic_interpolation_f64() {
+        let args: CubicInterpolationArgs<f64> = CubicInterpolationArgs::new(
+            [0.0, 0.25, 0.5, 0.75],
+            vec![
+                StepValue {
+                    resolution: 0.0,
+                    step_value: 0.0,
+                },
+                StepValue {
+                    resolution: 50.0,
+                    step_value: 100.0,
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: 200.0,
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0).round(), 67.0);
+        assert_eq!(expr.evaluate(60.0).round(), 158.0);
+    }
+
+    #[test]
+    fn cubic_interpolation_unordered() {
+        let args: CubicInterpolationArgs<Color> = CubicInterpolationArgs::new(
+            [0.0, 1.0, 0.5, 0.75],
+            vec![
+                StepValue {
+                    resolution: 50.0,
+                    step_value: Color::rgba(128, 128, 128, 128),
+                },
+                StepValue {
+                    resolution: 0.0,
+                    step_value: Color::rgba(0, 0, 0, 0),
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: Color::rgba(200, 200, 200, 200),
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0), Color::rgba(108, 108, 108, 108));
+        assert_eq!(expr.evaluate(60.0), Color::rgba(186, 186, 186, 186));
+    }
+
+    #[test]
+    fn cubic_interpolation_unordered_f64() {
+        let args: CubicInterpolationArgs<f64> = CubicInterpolationArgs::new(
+            [0.0, 0.25, 0.5, 0.75],
+            vec![
+                StepValue {
+                    resolution: 50.0,
+                    step_value: 100.0,
+                },
+                StepValue {
+                    resolution: 0.0,
+                    step_value: 0.0,
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: 200.0,
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0).round(), 67.0);
+        assert_eq!(expr.evaluate(60.0).round(), 158.0);
+    }
+
+    #[test]
+    fn cubic_interpolation_symmetric_control_points() {
+        let args: CubicInterpolationArgs<Color> = CubicInterpolationArgs::new(
+            [0.0, 1.0, 0.0, 1.0],
+            vec![
+                StepValue {
+                    resolution: 0.0,
+                    step_value: Color::rgba(0, 0, 0, 0),
+                },
+                StepValue {
+                    resolution: 50.0,
+                    step_value: Color::rgba(128, 128, 128, 128),
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: Color::rgba(200, 200, 200, 200),
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0), Color::rgba(126, 126, 126, 126));
+        assert_eq!(expr.evaluate(60.0), Color::rgba(198, 198, 198, 198));
+    }
+
+    #[test]
+    fn cubic_interpolation_symmetric_control_points_f64() {
+        let args: CubicInterpolationArgs<f64> = CubicInterpolationArgs::new(
+            [0.0, 1.0, 0.0, 1.0],
+            vec![
+                StepValue {
+                    resolution: 0.0,
+                    step_value: 0.0,
+                },
+                StepValue {
+                    resolution: 50.0,
+                    step_value: 100.0,
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: 200.0,
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0).round(), 99.0);
+        assert_eq!(expr.evaluate(60.0).round(), 198.0);
+    }
+
+    #[test]
+    fn cubic_interpolation_zeroes_control_points() {
+        let args: CubicInterpolationArgs<f64> = CubicInterpolationArgs::new(
+            [0.0, 0.0, 0.0, 0.0],
+            vec![
+                StepValue {
+                    resolution: 0.0,
+                    step_value: 0.0,
+                },
+                StepValue {
+                    resolution: 50.0,
+                    step_value: 100.0,
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: 200.0,
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0).round(), 50.0);
+        assert_eq!(expr.evaluate(60.0).round(), 140.0);
+    }
+
+    #[test]
+    fn cubic_interpolation_equal_control_points() {
+        let args: CubicInterpolationArgs<f64> = CubicInterpolationArgs::new(
+            [0.3, 0.3, 0.3, 0.3],
+            vec![
+                StepValue {
+                    resolution: 0.0,
+                    step_value: 0.0,
+                },
+                StepValue {
+                    resolution: 50.0,
+                    step_value: 100.0,
+                },
+                StepValue {
+                    resolution: 75.0,
+                    step_value: 200.0,
+                },
+            ]
+            .into_iter(),
+        )
+        .expect("failed to create interpolation arguments");
+
+        let expr = InterpolateExpression::new(InterpolationArgs::Cubic(args));
+
+        assert_eq!(expr.evaluate(25.0).round(), 50.0);
+        assert_eq!(expr.evaluate(60.0).round(), 140.0);
     }
 
     #[test]
